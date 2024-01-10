06b17c9c5312012b7a265a3a18459fe012cd91e1
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-01-10 13:41:36 +0900

    Title: 학점 계산기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240110/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(float, input().split()))
+
+hap = sum(arr[:])
+average = hap/n
+print(f'{average:.1f}')
+
+if average >= 4.0:
+    print('Perfect')
+elif average >= 3.0:
+    print('Good')
+else:
+    print('Poor')
