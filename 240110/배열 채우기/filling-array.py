c81375f764fc1e4651a450026601468c56a458e9
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-01-10 13:51:41 +0900

    Title: 배열 채우기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/배열 채우기/filling-array.py
+++ 240110/배열 채우기/filling-array.py
@@ -1,6 +1,8 @@
 arr = list(map(int, input().split()))
-arr = arr[::-1]
+cnt = 0
 for elem in arr:
     if elem == 0:
-        continue
-    print(elem, end=' ')
+        break
+    cnt += 1
+for i in range(cnt-1, -1, -1):
+    print(arr[i], end=' ')
