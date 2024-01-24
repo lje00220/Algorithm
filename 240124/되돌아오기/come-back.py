deb5627a2c4e699a744f51014f8eb32d112b40f9
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-01-24 23:46:07 +0900

    Title: 되돌아오기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240124/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+n = int(input())
+x, y = 0, 0
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+d = {
+    'E': 0,
+    'S': 1,
+    'W': 2,
+    'N': 3
+}
+cnt = 0
+answer = -1
+for i in range(n):
+    dir, dist = input().split()
+    dir_num, dist = d[dir], int(dist)
+
+    for _ in range(dist):
+        x += dx[dir_num]
+        y += dy[dir_num]
+        cnt += 1
+
+        if x == 0 and y == 0:
+            answer = cnt
+            break
+
+print(answer)
