0415ad1a7af374d0ec1214782aa81665147e542f
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-04-18 14:09:14 +0900

    Title: 입력받아 계산 5, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240418/입력받아 계산 5/input-calculate-5.js
+++ 240418/입력받아 계산 5/input-calculate-5.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split(" ");
-let a = arr[0];
-let b = arr[1];
+let a = Number(arr[0]);
+let b = Number(arr[1]);
 
 console.log(a+b);
