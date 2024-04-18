2b088a1c0b1c3a7e622214595d16564af7530133
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-04-18 14:15:17 +0900

    Title: 입력받은 값 교체하기, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240418/입력받은 값 교체하기/changing-inputs.js
+++ 240418/입력받은 값 교체하기/changing-inputs.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().split(" ");
+let arr = fs.readFileSync(0).toString().split(" ");
 let a = Number(arr[0]);
 let b = Number(arr[1]);
 
