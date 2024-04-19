fb855f72273500dd152ce9cef755babe72686498
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-04-19 15:17:56 +0900

    Title: 출석 부르기, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240419/출석 부르기/calling-attendance.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let num = Number(fs.readFileSync(0).toString().trim());
+
+if (num === 1) {
+    console.log("John");
+} else if (num === 2) {
+    console.log("Tom");
+} else if (num === 3) {
+    console.log("Paul");
+} else {
+    console.log("Vacancy");
+}
