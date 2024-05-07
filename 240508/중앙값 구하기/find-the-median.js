8d94aeca1e2a4197949cff1c2024d52efa3eaa51
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-05-08 02:08:58 +0900

    Title: 중앙값 구하기, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240508/중앙값 구하기/find-the-median.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+if (a > b) {
+    if (a < c) {
+        console.log(a);
+    } else if (b > c) {
+        console.log(b);
+    } else {
+        console.log(c);
+    }
+} else {
+    if (a > c) {
+        console.log(a);
+    } else if (b > c) {
+        console.log(c);
+    } else {
+        console.log(b);
+    }
+}
