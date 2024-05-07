5f7c937babdf8c5f0f87a624c6a52526e7720a15
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-05-08 01:58:48 +0900

    Title: 일 수 구하기, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240508/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+if (n <= 7) {
+    if (n == 2) {
+        console.log(28);
+    } else if (n % 2 == 0) {
+        console.log(30);
+    } else {
+        console.log(31);
+    }
+} else {
+    if (n % 2 == 0) {
+        console.log(31);
+    } else {
+        console.log(30);
+    }
+}
