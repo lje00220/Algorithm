fe4d9e29ba11853602c32e1cdd23bb9636812fef
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-05-22 17:06:54 +0900

    Title: a/b 출력, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240522/a/b 출력/a-divide-b.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let result = "";
+
+result += parseInt(a / b) + ".";
+
+a %= b;
+
+for (let i = 0; i < 20; i++) {
+    a *= 10;
+    result += parseInt(a / b);
+
+    a %= b;
+}
+
+console.log(result);
