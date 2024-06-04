9f7265ec3d511389dd84f817448f10a27e3911ce
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-06-04 14:55:16 +0900

    Title: 자연수면 출력, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240604/자연수면 출력/output-only-natural-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let result = "";
+
+if (a > 0) {
+    for (let i = 0; i < b; i++) {
+        result += a;
+    }
+} else {
+    console.log(0);
+}
+
+console.log(result);
