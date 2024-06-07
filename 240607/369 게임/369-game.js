8d92e522d828440d88b9288555d22057072fd44d
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-06-07 13:27:26 +0900

    Title: 369 게임, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240607/369 게임/369-game.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let result = "";
+
+for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0) {
+        result += "0 ";
+    } else if (i % 10 == 3 || i % 10 == 6 || i % 9 == 0) {
+        result += "0 ";
+    } else if (i / 10 == 3 || i / 10 == 6 || i / 9 == 0) {
+        result += "0 ";
+    } else {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
