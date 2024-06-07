00203a5a4e252258c259ca908e53b5d6eba3cff4
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-06-07 13:30:53 +0900

    Title: 369 게임, Time: 206ms, Memory: 15MB, Status: Passed - CodeTree

--- 240607/369 게임/369-game.js
+++ 240607/369 게임/369-game.js
@@ -5,9 +5,9 @@ let result = "";
 for (let i = 1; i <= n; i++) {
     if (i % 3 == 0) {
         result += "0 ";
-    } else if (i % 10 == 3 || i % 10 == 6 || i % 9 == 0) {
+    } else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
         result += "0 ";
-    } else if (i / 10 == 3 || i / 10 == 6 || i / 9 == 0) {
+    } else if (parseInt(i / 10) == 3 || parseInt(i / 10) == 6 || parseInt(i / 10) == 9) {
         result += "0 ";
     } else {
         result += (i + " ");
