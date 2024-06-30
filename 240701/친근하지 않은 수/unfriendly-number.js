29ae6d2730fe4dbbaa8c8cf6cdedc749d9def0df
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-01 02:26:03 +0900

    Title: 친근하지 않은 수, Time: 213ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240701/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 0;
+
+for(let i = 1; i <= n; i++) {
+    if (i % 2 == 0) {
+        continue;
+    }
+    if (i % 3 == 0) {
+        continue;
+    }
+    if (i % 5 == 0) {
+        continue;
+    }
+    cnt++;
+}
+
+console.log(cnt);
