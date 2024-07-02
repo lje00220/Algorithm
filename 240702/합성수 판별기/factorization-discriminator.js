46765e85808133a49931d59a9c192b0866d440e1
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-02 13:18:40 +0900

    Title: 합성수 판별기, Time: 197ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240702/합성수 판별기/factorization-discriminator.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let satisfied = false;
+
+for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+        satisfied = true;
+    }
+}
+
+if (satisfied) {
+    console.log("C")
+} else {
+    console.log("N");
+}
