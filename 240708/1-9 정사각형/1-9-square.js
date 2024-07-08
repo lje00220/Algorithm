a88c670793463137842ad4bdd5575efc7cb718fc
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-08 13:13:53 +0900

    Title: 1-9 정사각형, Time: 293ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240708/1-9 정사각형/1-9-square.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+let cnt = 1;
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += cnt;
+        cnt++;
+        if (cnt > 9) {
+            cnt = 1;
+        }
+    }
+    console.log(str);
+}
