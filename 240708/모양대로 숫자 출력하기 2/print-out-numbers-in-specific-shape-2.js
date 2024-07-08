9721a7088c0d3c41438a19a7a2e53d288d683d20
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-08 13:15:39 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240708/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+let cnt = 2;
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += cnt + " ";
+        cnt += 2;
+        if (cnt > 8) {
+            cnt = 2;
+        }
+    }
+    console.log(str);
+}
