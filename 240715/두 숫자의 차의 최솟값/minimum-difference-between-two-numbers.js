65220fa2cc75f1cccc5b54d50208c0d363e0d7b2
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-15 14:25:05 +0900

    Title: 두 숫자의 차의 최솟값, Time: 219ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240715/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+let min_val = 100;
+
+for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+        if (arr[i] > arr[j]) {
+            if (arr[i] - arr[j] < min_val) {
+                min_val = arr[i] - arr[j];
+            }
+        } else {
+            if (arr[j] - arr[i] < min_val) {
+                min_val = arr[j] - arr[i];
+            }
+        }
+    }
+}
+
+console.log(min_val);
