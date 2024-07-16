f737831bfe5acfe331348b33ed8b99c19f610ffe
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-16 17:19:26 +0900

    Title: 격자 반대로 채우기, Time: 220ms, Memory: 47MB, Status: Passed - CodeTree

--- 240716/격자 반대로 채우기/grid-anti-fill.js
+++ 240716/격자 반대로 채우기/grid-anti-fill.js
@@ -4,15 +4,17 @@ const n = Number(fs.readFileSync(0).toString().trim());
 let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
 
 let num = 1;
+let cnt = 1;
 for (let i = n - 1; i >= 0; i--) {
     for (let j = n - 1; j >= 0; j--) {
-        if (i % 2 !== 0) {
+        if (cnt % 2 !== 0) {
             arr2d[j][i] = num;
         } else {
             arr2d[n - j - 1][i] = num;
         }
         num++;
     }
+    cnt++;
 }
 
 for (let i = 0; i < n; i++) {
