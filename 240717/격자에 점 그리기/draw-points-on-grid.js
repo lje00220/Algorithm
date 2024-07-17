f1a99e75fc0a65bc72c03cb04e03f1f19f76865d
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-17 15:01:19 +0900

    Title: 격자에 점 그리기, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240717/격자에 점 그리기/draw-points-on-grid.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0].split(" ")[0]);
+let m = Number(input[0].split(" ")[1]);
+
+let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
+
+let num = 1;
+for (let i = 1; i <= m; i++) {
+    let r = Number(input[i].split(" ")[0]);
+    let c = Number(input[i].split(" ")[1]);
+
+    arr2d[r - 1][c - 1] = num;
+    num++;
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(arr2d[i].join(" "));
+}
