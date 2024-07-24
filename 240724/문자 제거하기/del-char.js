e97b234ec86af7ed28e945ffe7d6374fb087a3c4
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-24 16:20:04 +0900

    Title: 문자 제거하기, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240724/문자 제거하기/del-char.js
+++ 240724/문자 제거하기/del-char.js
@@ -5,7 +5,11 @@ let str = input[0].split("");
 for (let i = 1; i < input.length; i++) {
     if (str.length === 1) break;
     let n = input[i];
-    delete str[n];
+    if (n >= str.length - 1)  {
+        str.pop();
+    } else {
+        delete str[n];
+    }
 
     console.log(str.join(""));
 }
