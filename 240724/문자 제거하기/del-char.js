7fc3e3f65554e0ae3e247d961f3147f3269c3b14
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-07-24 16:28:45 +0900

    Title: 문자 제거하기, Time: 227ms, Memory: 14MB, Status: Passed - CodeTree

--- 240724/문자 제거하기/del-char.js
+++ 240724/문자 제거하기/del-char.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let str = input[0].split("");
+let str = input[0];
 
 for (let i = 1; i < input.length; i++) {
     if (str.length === 1) break;
-    let n = input[i];
+    let n = Number(input[i]);
     if (n >= str.length - 1)  {
-        str.pop();
+        str = str.slice(0, str.length - 1);
     } else {
-        delete str[n];
+        str = str.slice(0, n) + str.slice(n + 1);
     }
 
-    console.log(str.join(""));
+    console.log(str);
 }
