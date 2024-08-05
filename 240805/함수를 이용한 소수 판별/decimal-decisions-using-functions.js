90a5b7659e14c85dbc3f8279f5965926930f18a2
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-05 13:48:01 +0900

    Title: 함수를 이용한 소수 판별, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240805/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
+++ 240805/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 function isMagicNumber(n) {
+    if (n === 1) return false;
+
     for (let i = 2; i < n; i++) {
         if (n % i === 0) return false;
     }
