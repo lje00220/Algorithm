6e8abe4336d4b696f9f488270e586d7c861a0856
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-08 17:17:43 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 239ms, Memory: 14MB, Status: Passed - CodeTree

--- 240808/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 240808/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let n = Number(input[0]);
-let arr = input[1].split(" ").map(Number);
+let arr = Array(n + 1).fill(0);
+
+for (let i = 1; i <= n; i++) {
+    arr[i] = input[1].split(" ").map(Number)[i - 1];
+}
 
 function findMax(n) {
-    if (n === 0) {
-        return arr[0];
+    if (n === 1) {
+        return arr[1];
     }
 
-    return Math.max(findMax(n - 2), arr[n - 1]);
+    return Math.max(findMax(n - 1), arr[n]);
 }
 
 console.log(findMax(n));
