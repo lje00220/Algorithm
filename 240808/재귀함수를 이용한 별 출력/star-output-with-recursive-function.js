e2f7fd87bba5cf4ef25e777d058d2e09be86ed99
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-08 14:19:56 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 278ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240808/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+function printStar(n) {
+    if (n === 0) return;
+    printStar(n - 1);
+    for (let i = 0; i < n; i++) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
+
+printStar(n);
