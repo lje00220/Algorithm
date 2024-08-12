b9d3145ba29d6327c3434d00b27e9e7ce66c23cb
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-13 02:34:20 +0900

    Title: 개인정보, Time: 172ms, Memory: 14MB, Status: Passed - CodeTree

--- 240813/개인정보/personal-info.js
+++ 240813/개인정보/personal-info.js
@@ -11,7 +11,7 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let people = [];
 
-for (let i = 0; i < input.length; i++) {
+for (let i = 0; i < 5; i++) {
     let [name, height, weight] = input[i].trim().split(" ");
     people.push(new Person(name, Number(height), Number(weight)));
 }
@@ -29,7 +29,7 @@ people.sort(cmp);
 
 console.log("name");
 people.forEach((person) => {
-    console.log(person.name, person.height, person.weight);
+    console.log(person.name, person.height, person.weight.toFixed(1));
 });
 
 people.sort((prev, cur) => cur.height - prev.height);
@@ -37,5 +37,5 @@ people.sort((prev, cur) => cur.height - prev.height);
 console.log();
 console.log("height");
 people.forEach((person) => {
-    console.log(person.name, person.height, person.weight);
+    console.log(person.name, person.height, person.weight.toFixed(1));
 });
