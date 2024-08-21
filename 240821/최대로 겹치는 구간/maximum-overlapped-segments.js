e533bb84d227c77790eb3b4dab3d0232fb8d45fd
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-21 20:48:30 +0900

    Title: 최대로 겹치는 구간, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240821/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 240821/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -6,7 +6,7 @@ let arr = Array(200).fill(0);
 
 for (let i = 1; i <= n; i++) {
     let [x1, x2] = input[i].trim().split(" ").map(Number);
-    for (let i = x1 + 1; i < x2; i++) {
+    for (let i = x1; i < x2; i++) {
         arr[i] += 1;
     }
 }
