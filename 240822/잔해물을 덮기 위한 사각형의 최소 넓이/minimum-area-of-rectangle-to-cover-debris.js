b99ad89f9d44d44c264979ad8605455f9bf78ce6
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-22 18:42:50 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 375ms, Memory: 60MB, Status: Wrong Answer - CodeTree

--- 240822/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 240822/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -10,22 +10,30 @@ for (let i = 0; i < 2; i++) {
     let [x1, y1, x2, y2] = input[i].trim().split(" ").map(Number);
     x1 += OFFSET; x2 += OFFSET;
     y1 += OFFSET; y2 += OFFSET;
-    for (let j = x1; j <= x2; j++) {
-        for (let k = y1; k <= y2; k++) {
+    for (let j = x1; j < x2; j++) {
+        for (let k = y1; k < y2; k++) {
             arr2d[j][k] = index;
         }
     }
     index += 1;
 }
 
-let area = 0;
+//x와 y의 최댓값을 구해야 함
+
+let [maxX, maxY] = [0, 0];
+let [minX, minY] = [MAX, MAX];
 for (let i = 0; i < MAX; i++) {
     for (let j = 0; j < MAX; j++) {
         if (arr2d[i][j] === 1) {
-            area += 1;
-            // console.log(i, j);
+            if (i >= maxX) maxX = i;
+            if (j >= maxY) maxY = j;
+            if (i <= minX) minX = i - 1;
+            if (j <= minY) minY = j - 1;
         }
     }
 }
+// console.log(maxX, maxY);
+// console.log(minX, minY);
 
+let area = (maxX - minX) * (maxY - minY);
 console.log(area);
