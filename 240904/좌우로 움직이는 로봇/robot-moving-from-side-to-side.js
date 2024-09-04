6696a857e8c4d03df8b6108e69007409072672b1
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-09-04 16:19:22 +0900

    Title: 좌우로 움직이는 로봇, Time: 434ms, Memory: 72MB, Status: Passed - CodeTree

--- 240904/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
+++ 240904/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -13,12 +13,12 @@ for (let i = 1; i <= n; i++) {
     time = Number(time);
 
     for (let j = 0; j < time; j++) {
-        robotA.push(currentA);
         if (dir === "R") {
             currentA++;
         } else {
             currentA--;
         }
+        robotA.push(currentA);
     }
 }
 
@@ -27,19 +27,30 @@ for (let i = n + 1; i < input.length; i++) {
     time = Number(time);
 
     for (let j = 0; j < time; j++) {
-        robotB.push(currentB);
         if (dir === "R") {
             currentB++;
         } else {
             currentB--;
         }
+        robotB.push(currentB);
     }
 }
 
 let cnt = 0;
-for (let i = 1; i <= Math.max(robotA.length, robotB.length); i++) {
+let last = robotA.length < robotB.length ? robotA[robotA.length - 1] : robotB[robotB.length - 1];
+let standard = robotA.length < robotB.length ? robotA.length : robotB.length;
+
+for (let i = 1; i <= standard; i++) {
     if (robotA[i - 1] !== robotB[i - 1] && robotA[i] === robotB[i]) {
         cnt++;
+    }
+}
+
+for (let i = standard; i <= Math.max(robotA.length, robotB.length); i++) {
+    if (robotA.length > robotB.length) {
+        if (last === robotA[i]) cnt++;
+    } else {
+        if(last === robotB[i]) cnt++;
     }
 }
 
