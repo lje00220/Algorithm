d2e08e0fa62d66f32d703e2d33faa64bbb89c6cf
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-09-13 23:49:24 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 173ms, Memory: 18MB, Status: Passed - CodeTree

--- 240913/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
+++ 240913/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -3,15 +3,20 @@ let [n, m] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let arr = Array(n).fill(0).map(() => Array(m).fill(0));
 arr[0][0] = 1;
-let dx = [0, 1, 0, -1], dy = [-1, 0, 1, 0];
+let dx = [1, 0, -1, 0], dy = [0, 1, 0, -1];
 let dirNum = 0;
 let [x, y] = [0, 0];
 
 for (let i = 2; i <= n * m; i++) {
     let nx = x + dx[dirNum], ny = y + dy[dirNum];
 
-    if (!inRange(nx, ny) || arr[nx][ny] !== 0) {
-        dirNum = (dirNum + 1) % 4;
+    while (true) {
+        if (!inRange(nx, ny) || arr[nx][ny] !== 0) {
+            dirNum = (dirNum + 1) % 4;
+            nx = x + dx[dirNum], ny = y + dy[dirNum];
+        } else {
+            break;
+        }
     }
 
     x += dx[dirNum], y += dy[dirNum];
