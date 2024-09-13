f2362311d01e58603e613f52b84fa9d7279ace17
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-09-13 23:30:14 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240913/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+let [n, m] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let arr = Array(n).fill(0).map(() => Array(m).fill(0));
+arr[0][0] = 1;
+let dx = [0, 1, 0, -1], dy = [-1, 0, 1, 0];
+let dirNum = 0;
+let [x, y] = [0, 0];
+
+for (let i = 2; i <= n * m; i++) {
+    let nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+    if (!inRange(nx, ny) || arr[nx][ny] !== 0) {
+        dirNum = (dirNum + 1) % 4;
+    }
+
+    x += dx[dirNum], y += dy[dirNum];
+    arr[x][y] = i;
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(arr[i].join(" "));
+}
+
+function inRange(x, y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
