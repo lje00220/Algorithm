748e89fb1452aab5e01664d266a228efe214d8e9
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-01-31 16:30:33 +0900

    [level 1] Title: 소수 찾기, Time: 62.22 ms, Memory: 45.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let primes = new Array(n + 1).fill(true); 
+    primes[0] = primes[1] = false; 
+
+    for (let i = 2; i * i <= n; i++) {  
+        if (primes[i]) {  
+            for (let j = i * i; j <= n; j += i) {  
+                primes[j] = false;
+            }
+        }
+    }
+    
+    return primes.filter((prime) => prime === true).length;
+}
