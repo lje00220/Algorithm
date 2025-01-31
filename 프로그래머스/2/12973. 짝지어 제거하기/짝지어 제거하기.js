c3f2392833a74cde547253b6aec62c4c4ba1ff4e
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-01-31 19:17:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 70.20 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,6 +1,5 @@
 function solution(s) {
     const result = s.split("").reduce((acc, cur, idx) => {
-        if (acc.length === 0) return [cur];
         if (acc[acc.length - 1] !== cur) {
             acc.push(cur);
         } else {
