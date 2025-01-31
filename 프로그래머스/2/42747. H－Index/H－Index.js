9dcfad262ab0f1da61da4159f6e95a62ebbaea92
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-01-31 19:28:52 +0900

    [level 2] Title: H-Index, Time: 144.91 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,7 +1,7 @@
 function solution(citations) {
     let answer = 0;
     for (let i = 0; i <= Math.max(...citations); i++) {
-        const temp = citations.map((citation) => citation >= i).filter((x) => x)
+        const temp = citations.filter((x) => x >= i)
         if (temp.length >= i) answer = i;
     }
     return answer;
