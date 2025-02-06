3a549beb67a14fc50544e51e32ffb745756519e1
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-02-06 09:17:22 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 172 ms, Memory: 10796 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const [a, b, c, d, e, f] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+for (let x = -999; x < 1000; x++) {
+  for (let y = -999; y < 1000; y++) {
+    if (a * x + b * y === c && d * x + e * y === f) {
+      console.log(x, y);
+    }
+  }
+}
