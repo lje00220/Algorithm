be800e418b7c28bab1708c6649b78fe793cfcef6
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-02-06 09:38:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3992 ms, Memory: 67692 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const result = [];
+
+for (let i = 1; i < input.length; i++) {
+  const [a, b] = input[i].split(" ");
+  result.push([a, b]);
+}
+
+result.sort((x, y) => {
+  if (+x[0] > +y[0]) {
+    return 1;
+  }
+  if (+x[0] < +y[0]) {
+    return -1;
+  }
+  if (+x[1] > +y[1]) {
+    return 1;
+  }
+  return -1;
+});
+
+result.forEach((x) => console.log(x.join(" ")));
