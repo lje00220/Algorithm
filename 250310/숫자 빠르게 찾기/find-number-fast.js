5778a5cda8a20d6ddf532954123804b606a42ba5
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-03-10 09:41:55 +0900

    Title: 숫자 빠르게 찾기, Time: 5095ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 250310/숫자 빠르게 찾기/find-number-fast.js
+++ 250310/숫자 빠르게 찾기/find-number-fast.js
@@ -5,11 +5,15 @@ const [n, m] = input[0].split(' ').map(Number);
 const numbers = input[1].split(' ').map(Number);
 const queries = input.slice(2, 2 + m).map(Number);
 
+const result = [];
+
 for (let i = 0; i < queries.length; i++) {
     const idx = numbers.indexOf(queries[i]);
     if (idx < 0) {
-        console.log(-1);
+        result.push(-1);
     } else {
-        console.log(idx + 1);
+        result.push(idx + 1);
     }
 }
+
+console.log(result.join("\n"))
