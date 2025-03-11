078abe89b098e178db36ca9c32598bbe75e5f975
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-03-11 09:29:51 +0900

    Title: 가장 먼저 나오는 숫자, Time: 5067ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

---
+++ 250311/가장 먼저 나오는 숫자/first-appear-number.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+const queries = input[2].split(' ').map(Number);
+
+const lower_bound = (target) => {
+    let [left, right, min_idx] = [0, N - 1, N];
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] >= target) {
+            min_idx = Math.min(min_idx, mid);
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return min_idx;
+}
+
+for (let i = 0; i < M; i++) {
+    if (!arr.includes(queries[i])) {
+        console.log(-1);
+    } else {
+    console.log(lower_bound(queries[i]) + 1);
+    }
+}
