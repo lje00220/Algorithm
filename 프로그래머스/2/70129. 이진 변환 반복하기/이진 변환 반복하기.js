af344294938f593f2f23a2fb9380ab869fefd574
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-03-25 09:52:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 30.34 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,11 @@
 function solution(s) {
-    let [cnt, cnt0] = [0, 0];
+    let [cnt, zeroCnt] = [0, 0];
     while (s !== "1") {
-        // 1단계) 0 제거
-        cnt0 += s.split("").filter((x) => x === "0").length;
-        s = s.replaceAll("0", "");
-        
-        // 2단계) 이진 변환
-        s = s.length.toString(2);
+        zeroCnt += s.split("").filter((x) => x === "0").length;
+        const str = s.replaceAll("0", "");
+        s = parseInt(str.length).toString(2);
         cnt++;
-    }
-    return [cnt, cnt0];
+    } 
+    
+    return [cnt, zeroCnt];
 }
