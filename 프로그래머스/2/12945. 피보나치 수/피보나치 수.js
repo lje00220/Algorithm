7de53c654e493fdc192eda969f538c8849dec27c
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-03-31 09:10:03 +0900

    [level 2] Title: 피보나치 수, Time: 1.69 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,8 +1,12 @@
 function solution(n) {
-    const arr = [0, 1];
-    for (let i = 2; i <= n; i++) {
-        arr.push(arr[i - 1] % 1234567 + arr[i - 2] % 1234567);
+    let answer = 0;
+    let [f0, f1] = [0, 1];
+    
+    for(let i = 2; i <= n; i++){
+        answer = (f0 + f1) % 1234567;
+        f0 = f1;
+        f1 = answer;
     }
-    const answer = arr[arr.length - 1] % 1234567;
+    
     return answer;
 }
