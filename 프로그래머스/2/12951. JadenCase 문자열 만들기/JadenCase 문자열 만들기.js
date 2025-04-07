37e3fdc2f8101662dd7fcec5f676c96772b907bb
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-04-07 17:24:35 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,19 +1,9 @@
 function solution(s) {
-    var answer = '';
-    s = s.split(" ");
-    for (let i = 0; i < s.length; i++) {
-        if (s[i][0] >= "a" && s[i][0] <= "z" || s[i][0] >= "A" && s[i][0] <= "Z") {
-            s[i] = s[i].toLowerCase();
-            let temp = s[i].split("");
-            temp[0] = temp[0].toUpperCase();
-            answer += temp.join("")
-            
-        } else {
-            s[i] = s[i].toLowerCase();
-            answer += s[i];
-        }
-        answer += (i === s.length - 1) ? "" : " "
-    
+    const arr = s.split(" ");
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const temp = arr[i].split("").map((x, idx) => idx === 0 ? x.toUpperCase() : x.toLowerCase()).join("");
+        result.push(temp);
     }
-    return answer;
+    return result.join(" ");
 }
