d48566ee59a61584c4b9dd2fe67fcee68a97d06e
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-04-18 18:27:01 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,11 +1,13 @@
-function solution(arr) {   
-    const gcd = (a,b) => a % b === 0 ? b : gcd(b, a % b); 
-    const lcm = (a,b) => a * b / gcd(a,b);
-    
-    let leastNum = 1;
-    for (let i = 0; i < arr.length; i++) {
-        leastNum = lcm(arr[i], leastNum);
+function solution(arr) {
+    let answer = 0;
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    const lcm = (a, b) => a * b / gcd(a, b);
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (i === 0) {
+            answer = lcm(arr[i], arr[i + 1]);
+        } else {
+            answer = lcm(answer, arr[i + 1]);
+        }
     }
-    
-    return leastNum;
+    return answer;
 }
