8a06ca8495de44fee0d75c5f7ebbe9795bea2fea
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-04-29 20:47:14 +0900

    [level 2] Title: 구명보트, Time: 32.01 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,14 +1,17 @@
 function solution(people, limit) {
-    const sortedArr = people.sort((x, y) => x - y);
-    let cnt = 0;
-    while (sortedArr.length !== 0) {
-        if (sortedArr[0] + sortedArr[sortedArr.length - 1] <= limit) {
-            sortedArr.shift();
-            sortedArr.pop();
-        } else {
-            sortedArr.pop();
-        }
-        cnt++;
+  people.sort((a, b) => a - b);
+
+  let start = 0;
+  let end = people.length - 1;
+  let boats = 0;
+
+  while (start <= end) {
+    if (people[start] + people[end] <= limit) {
+      start++;
     }
-    return cnt;
+    end--;
+    boats++;
+  }
+
+  return boats;
 }
