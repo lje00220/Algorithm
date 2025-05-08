4872f1dd8c22ca01d0850e29783c5645761e4cc4
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-05-08 14:43:59 +0900

    [level 1] Title: 문자열 나누기, Time: 0.95 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,19 +1,15 @@
 function solution(s) {
     let answer = 0;
-    let [xCnt, notXCnt] = [0, 0];
-    s = s.split("");
     let x = s[0];
+    let cnt = 0;
     for (let i = 0; i < s.length; i++) {
-        if (x === s[i]) xCnt++;
-        if (x !== s[i]) notXCnt++;
-        if (xCnt === notXCnt) {
+        cnt = x === s[i] ? cnt + 1 : cnt - 1;
+        
+        if (cnt === 0) {
             answer++;
-            [xCnt, notXCnt] = [0, 0];
             x = s[i + 1];
         }
-        
-        if ((i === s.length - 1) && (xCnt !== notXCnt)) answer++;
-        
     }
+    if (cnt !== 0) answer++;
     return answer;
 }
