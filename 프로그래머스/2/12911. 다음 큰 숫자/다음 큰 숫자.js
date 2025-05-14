ca2761406abaa97fada6ffa01f0a35850750f979
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-05-14 20:17:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,8 +1,7 @@
 function solution(n) {
-    const numOne = n.toString(2).split("").filter((x) => x === "1").length;
-    while(true) {
+    const cnt1 = n.toString(2).replaceAll('0', '').length;
+    while (true) {
         n++;
-        const cnt0 = n.toString(2).split("").filter((x) => x === "1").length;
-        if (cnt0 === numOne) return n;
+        if (n.toString(2).replaceAll('0', '').length === cnt1) return n;
     }
 }
