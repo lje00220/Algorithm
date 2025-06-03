35d307b98a9fecf2a21b621bbe4bdb924ec3a3a5
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-03 18:44:17 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.64 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,17 +1,12 @@
 function solution(k, score) {
   let answer = [];
-  let honor = []; 
-  
+  const honors = [];
   for (let i = 0; i < score.length; i++) {
-    honor.push(score[i]);
-    honor.sort((a, b) => b - a);
-    
-    if (honor.length > k) {
-      honor.pop(); 
-    }
-    
-    answer.push(honor[honor.length - 1]); 
+    honors.push(score[i])
+    honors.sort((a, b) => b - a);
+    if (i >= k) honors.pop();
+    answer.push(honors[honors.length - 1])
   }
-  
+
   return answer;
 }
