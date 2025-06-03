45d69301509b3fca7bf9aac4982f450f08014e80
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-03 18:25:03 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,7 @@
 function solution(n) {
-  const dp = [0, 1, 2];
-
-  for (let i = 3; i <= n; i++) {
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-  }
-
-  return dp[n];
+    const arr = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    }
+    return arr[n];
 }
