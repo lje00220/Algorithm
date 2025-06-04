1537bb109fdbdce3307d220141cca491612fc4c3
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-04 12:27:40 +0900

    [level 2] Title: 가장 큰 수, Time: 163.67 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,8 +1,4 @@
 function solution(numbers) {
-  const result = numbers
-    .map(String)
-    .sort((a, b) => (b + a) - (a + b))
-    .join('');
-
-  return result[0] === '0' ? '0' : result;
+    const result = numbers.map(String).sort((a, b) => (b + a) - (a + b)).join("");
+    return result[0] === '0'? '0' : result;
 }
