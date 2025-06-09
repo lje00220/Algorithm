e331bb127b6d8038e0c9da8a9768f0b07522e8bc
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-09 15:52:48 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 29.60 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,16 +1,16 @@
 function solution(numbers) {
     const answer = Array(numbers.length).fill(-1);
-    const stack = [];
-
+    const stack = []
     for (let i = numbers.length - 1; i >= 0; i--) {
         while (stack.length && stack[stack.length - 1] <= numbers[i]) {
             stack.pop();
         }
-        if (stack.length) {
-            answer[i] = stack[stack.length - 1];
+        const len = stack.length;
+        if (len) {
+            answer[i] = stack[len - 1];
         }
         stack.push(numbers[i]);
     }
-
+    
     return answer;
 }
