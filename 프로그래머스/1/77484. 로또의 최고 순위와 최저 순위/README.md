3854864350043ee5f0908851068c49d3dca8d3f4
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-13 13:50:03 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#fn1) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.11 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 08일 10:54:01
+2025년 06월 13일 13:49:59
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,12 +1,12 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    let [zeroNum, winning] = [0, 0]
-    lottos.forEach((x) => {
-        if (win_nums.includes(x)) winning++;
-        if (x === 0) zeroNum++;
-    })
-    7 - (winning + zeroNum) == 7 ? answer.push(6) : answer.push(7 - (winning + zeroNum));
-    (7 - winning) === 7 ? answer.push(6) : answer.push(7 - winning);
-    
-    return answer;
+    const filteredLottos = lottos.filter((lotto) => lotto !== 0);
+    const zeroNum = 6 - filteredLottos.length;
+    let answer = 0;
+    for (let i = 0; i < filteredLottos.length; i++) {
+        if (win_nums.includes(filteredLottos[i])) answer++;
+    }
+    const upperRank = (answer + zeroNum) < 2 ? 6 : 7 - (answer + zeroNum);
+    const lowerRank = answer === 0 ? 6 : 7 - answer;
+          
+    return [upperRank, lowerRank];
 }
