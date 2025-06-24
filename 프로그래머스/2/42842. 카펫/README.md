7b2a0fef072f97dee6632d1a72ad6dc47fc59e18
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-06-24 12:38:51 +0900

    [level 2] Title: 카펫, Time: 5.05 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 7.99 ms
+메모리: 36.7 MB, 시간: 5.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 15일 23:13:50
+2025년 06월 24일 12:38:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,10 +2,10 @@ function solution(brown, yellow) {
     const addNum = brown + yellow;
     const result = [];
     if (Number.isInteger(Math.sqrt(addNum))) return [Math.sqrt(addNum), Math.sqrt(addNum)];
-    let index = 1;
-    while (index <= addNum / 2) {
-      if (addNum % index === 0) result.push(index)
-      index++
+    let idx = 1;
+    while (idx <= addNum / 2) {
+      if (addNum % idx === 0) result.push(idx)
+      idx++
     }
     result.push(addNum);
     for (let i = 0; i < result.length; i++) {
