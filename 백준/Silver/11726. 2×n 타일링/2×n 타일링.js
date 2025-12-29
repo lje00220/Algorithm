f33077b779f8c7b2b9a1b78904a2bb3d19c01fda
Jieun Lee <155710708+lje00220@users.noreply.github.com>
2025-12-29 22:09:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 156 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+const arr = new Array(n + 1);
+arr[1] = 1;
+arr[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+}
+
+console.log(arr[n]);
